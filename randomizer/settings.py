001118214a970ffac9aad3bf10757f2eb46304c9
Nickolaus Smith <nickolaussmith721@gmail.com>
2018-11-13 14:18:00 -0700

    Whitenoise and django-heroku added

--- randomizer/settings.py
+++ randomizer/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import dj_database_url
 from decouple import config
-
+import whitenoise
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -145,3 +145,6 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+import django_heroku
+django_heroku.settings(locals())
