2ab6ef6dde9f35b6e14c9bd5e585748705dba5cd
sulemc <susanna.mcdo@gmail.com>
2018-11-14 12:43:01 -0600

    working last mod

--- clss/admin.py
+++ clss/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from .models import Clss
 # Register your models here.
 
-admin.site.register(Clss)
+
+class ClssAdmin(admin.ModelAdmin):
+    readonly_fields=('created_at','last_modified')
+
+
+admin.site.register(Clss, ClssAdmin)
--- clss/models.py
+++ clss/models.py
@@ -8,4 +8,3 @@ class Clss(models.Model):
     #total_average = models.DecimalField(..., max_digits=6, decimal_places=1)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-
