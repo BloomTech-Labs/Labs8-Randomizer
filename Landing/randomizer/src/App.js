efa4d95956cd6ddf8dfcaf2e45bb32824023d060
Nickolaus Smith <nickolaussmith721@gmail.com>
2018-11-26 16:04:56 -0700

    Rechart added, shell for endpoints being built out, updated favicon and name of react app

--- Landing/randomizer/src/App.js
+++ Landing/randomizer/src/App.js
@@ -15,7 +15,7 @@ import Billing from './Components/Billing/billing';
 import GoogleApi from './Components/GoogleApi/GoogleApi';
 import Newuser from './Components/Randomuser/Newuser';
 import Class from './Components/Classes/Class';
-
+import Chart from './Components/Rechart/Charts';
 // Icons
 import Vert from '@material-ui/icons/Reorder';
 import Gearicon from '@material-ui/icons/Settings';
@@ -182,6 +182,7 @@ class App extends Component {
        <Route path = '/Google-class' component={GoogleApi} />
         <Route exact path = '/Newuser' component={Newuser}/>
         <Route exact path = '/Classes' component={Class}/>
+        <Route exact path = '/Chart' component={Chart}/>
       </Appmain>
       </Background>
     );
