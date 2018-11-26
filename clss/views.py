02515710a9efd9deec8cbc2e3177f69158fa729e
sulemc <susanna.mcdo@gmail.com>
2018-11-26 16:54:48 -0600

    brady fix, can now dynamically create classes

--- clss/views.py
+++ clss/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 from django.http import JsonResponse
 from django.contrib.auth.models import User
 from .models import ClssName, StudentName, Participation
 from django.core.exceptions import ValidationError
-from django_currentuser.middleware import (
-    get_current_user, get_current_authenticated_user)
+#from django_currentuser.middleware import (
+    #get_current_user, get_current_authenticated_user)
 import json
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import permissions
+
 
 @csrf_exempt
+@api_view(["POST"])
+@permission_classes((permissions.AllowAny,))
 def createClass(request):
     data = json.loads(request.body)
     name_of_class = data['class_name']
-    user = User.objects.get(pk=2)
+    user = request.user
+    print(user.username)
     newClass2 = ClssName.manager.create_class(user,name_of_class)
     newClass2.save()
     response = JsonResponse({"id":str(newClass2.id)}, safe=True, status=201)
