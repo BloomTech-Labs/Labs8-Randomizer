b49858ed1333e04b5f04229d79b9e778ef8154d9
emaquino44 <prexy44@yahoo.com>
2018-11-28 12:02:40 -0800

    Add Google OAuth

--- api/views.py
+++ api/views.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 import json
 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import permissions
+
+
 @csrf_exempt
 def register(request):
     data = json.loads(request.body)
@@ -45,3 +49,49 @@ def login(request):
             response = JsonResponse({"error":"Unable to log in with provided credentials."}, safe=True, status=500)
     return response
 
+@csrf_exempt
+@api_view(["POST"])
+@permission_classes((permissions.AllowAny,))
+def tokenRegister(request):
+    data = json.loads(request.body)
+    print('here again', data)
+    username = data['username']
+    email = data['email']
+    user=User(username=username)
+    try:
+      user.validate_unique()
+    except ValidationError:
+          response = JsonResponse({"error":"A user with that username already exists."}, safe=True, status=500)
+    else:
+          user.save()
+          response = JsonResponse({"key":str(user.auth_token)}, safe=True, status=201)
+    return response
+
+
+
+
+
+
+# (Receive token by HTTPS POST)
+
+# try:
+#     # Specify the CLIENT_ID of the app that accesses the backend:
+#     idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+
+#     # Or, if multiple clients access the backend server:
+#     # idinfo = id_token.verify_oauth2_token(token, requests.Request())
+#     # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
+#     #     raise ValueError('Could not verify audience.')
+
+#     if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+#         raise ValueError('Wrong issuer.')
+
+#     # If auth request is from a G Suite domain:
+#     # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
+#     #     raise ValueError('Wrong hosted domain.')
+
+#     # ID token is valid. Get the user's Google Account ID from the decoded token.
+#     userid = idinfo['sub']
+# except ValueError:
+#     # Invalid token
+#     pass
