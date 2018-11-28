fb1072cd615ecb8c3b06986fbc71625be476e185
sulemc <susanna.mcdo@gmail.com>
2018-11-27 19:10:50 -0600

    added all JSON responses

--- clss/urls.py
+++ clss/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('class_list', views.createClass),
+    url('create_class', views.createClass), #POST request
+    url('class_list', views.listClass), #GET request
     url('add_student', views.createStudent),
-    url('participate', views.createParticipation)
+    url('list_students', views.studentList),
+    url('participate', views.createParticipation),
+    url('participation_list', views.particpationTotal)
 ]
