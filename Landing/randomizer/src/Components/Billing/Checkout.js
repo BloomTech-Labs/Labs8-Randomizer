549c0fa4fcbf1c855890807011334c25d9b8f9ea
Nickolaus Smith <nickolaussmith721@gmail.com>
2018-11-29 11:05:03 -0700

    Stripe now in test mode

--- Landing/randomizer/src/Components/Billing/Checkout.js
+++ Landing/randomizer/src/Components/Billing/Checkout.js
@@ -2,10 +2,10 @@ import React from 'react'
 import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
 
-import STRIPE_PUBLISHABLE from './constants/stripe';
+
 import PAYMENT_SERVER_URL from './constants/server';
 
-const CURRENCY = 'NA';
+const CURRENCY = 'USD';
 
 const fromEuroToCent = amount => amount * 100;
 
@@ -35,7 +35,7 @@ const Checkout = ({ name, description, amount }) =>
     amount={fromEuroToCent(amount)}
     token={onToken(amount, description)}
     currency={CURRENCY}
-    stripeKey={STRIPE_PUBLISHABLE}
+    stripeKey={'pk_test_W5zaThxw6j82sZI0LwET9aQK'}
   />
 
 export default Checkout;
