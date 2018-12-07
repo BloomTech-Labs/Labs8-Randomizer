8f975daec536855076380cb168b6d0e01e6846b2
sulemc <susanna.mcdo@gmail.com>
2018-12-07 10:30:16 -0600

    added runtime

--- randomizer/urls.py
+++ randomizer/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-
 from rest_framework import routers
 #from clss.api import ClssViewSet
 
